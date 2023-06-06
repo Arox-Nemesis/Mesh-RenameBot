99575640c0f0a9e7151cd3762adddc7440f849e8
Arox-Nemesis <135242263+Arox-Nemesis@users.noreply.github.com>
2023-06-06 14:33:57 +0530

    Update config.py

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -12,7 +12,7 @@ except ImportError:
         REMAINING_STR = [str, "▱"]
         MAX_QUEUE_SIZE = [int, 50]
         SLEEP_SECS = [int, 0]
-        IS_MONGO = [bool, False]
+        IS_MONGO = [bool, True]
 
         # Access Restriction
         IS_PRIVATE = [bool, False]
